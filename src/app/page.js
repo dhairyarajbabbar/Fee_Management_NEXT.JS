d7abea2b6a755839203ffb3c4e7022de030bfca7
dhairyarajbabbar <dhairyarajbabbar@gmail.com>
2024-05-17 17:43:01 +0530

    redirect added if the accesstoken is present or not

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
-'use client'
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("accessToken");
+  let redirectPath = "/login";
 
-  return (
-    <>
-    </>
-  );
+  console.log("token by me : ", token);
+  if (token.value) {
+    redirectPath = "/students";
+  }
+  if (redirectPath) redirect(redirectPath);
+  return <>home</>;
 }
